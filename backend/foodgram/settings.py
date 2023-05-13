639417d15a01be4fe7c4602a670a480c3eee42cb
Андрей Борзенко <borzenkoandreys@yandex.ru>
2023-05-13 18:39:40 +0300

    Фронт в игноре

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -129,7 +129,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
-DEFAULT_FROM_EMAIL = 'api@yamdb.ru'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
