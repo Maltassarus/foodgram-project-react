e84c95cf44541daff0031a03ec92d2502de6174c
Андрей Борзенко <borzenkoandreys@yandex.ru>
2023-06-07 22:48:22 +0300

    Мелкая правка

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -43,24 +43,3 @@ class IsAdminOrSuperuser(permissions.BasePermission):
             request.user.is_authenticated
             and (request.user.is_admin or request.user.is_superuser)
         )
-
-
-class IsCanChangeOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
-
-    def has_object_permission(self, request, view, obj):
-        is_safe_method = request.method in permissions.SAFE_METHODS
-        is_correct_user = (
-            request.user.is_authenticated
-            and (
-                request.user.is_superuser
-                or request.user.is_admin
-                or request.user.is_moderator
-                or obj.author == request.user
-            )
-        )
-        return is_safe_method or is_correct_user
