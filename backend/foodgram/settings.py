60e5c4a7a85d369537dc25adfa5269e62eb2b486
Андрей Борзенко <borzenkoandreys@yandex.ru>
2023-06-17 08:22:36 +0300

    Убран дебаг

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', default=12345)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['130.193.42.245',
                  'localhost',
