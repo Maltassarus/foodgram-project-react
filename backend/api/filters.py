5e7835faeb13fb07430ff14cbe89e64343b1564c
Андрей Борзенко <borzenkoandreys@yandex.ru>
2023-06-18 11:12:56 +0300

    правка фильтров

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -6,32 +6,45 @@ from users.models import User
 
 
 class IngredientSearchFilter(SearchFilter):
-    search_param = 'name'
+    name = rf_filters.CharFilter(method='startswith_contains_union_method')
+
+    class Meta:
+        model = Ingredient
+        fields = ['name']
+
+    def startswith_contains_union_method(self, queryset, name, value):
+        if not bool(value):
+            return queryset
+        startswith_lookup = '__'.join([name, 'istartswith'])
+        contains_lookup = '__'.join([name, 'icontains'])
+        return queryset.filter(
+            Q(**{startswith_lookup: value}) | Q(**{contains_lookup: value})
+        ).annotate(
+            is_start=ExpressionWrapper(
+                Q(**{startswith_lookup: value}),
+                output_field=BooleanField()
+            )
+        ).order_by('-is_start')
 
 
 class RecipeFilter(FilterSet):
-    author = filters.ModelChoiceFilter(
-        queryset=User.objects.all())
-    tags = filters.ModelMultipleChoiceFilter(
-        field_name='tags__slug',
-        to_field_name='slug',
-        queryset=Tag.objects.all(),
-    )
-    is_in_shopping_cart = filters.NumberFilter(
-        method='filter_is_in_shopping_cart')
-    is_favorited = filters.NumberFilter(
-        method='filter_is_favorited')
+    tags = rf_filters.AllValuesMultipleFilter(field_name='tags__slug')
+    is_favorited = rf_filters.NumberFilter(method='recipe_boolean_methods')
+    is_in_shopping_cart = rf_filters.NumberFilter(
+        method='recipe_boolean_methods')
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+        fields = ['tags', 'author', 'is_favorited', 'is_in_shopping_cart']
 
-    def filter_is_favorited(self, queryset, name, value):
-        if self.request.user.is_authenticated and value:
-            return queryset.filter(favorite__author=self.request.user)
-        return queryset
-
-    def filter_is_in_shopping_cart(self, queryset, name, value):
-        if self.request.user.is_authenticated and value:
-            return queryset.filter(shopping_cart__author=self.request.user)
-        return queryset
+    def recipe_boolean_methods(self, queryset, name, value):
+        if not bool(value):
+            return queryset
+        user = self.request.user
+        if user.is_anonymous:
+            return queryset
+        recipe_ids = [
+            r.pk for r in queryset if getattr(r, name)(user) == value]
+        if recipe_ids:
+            return queryset.filter(pk__in=recipe_ids)
+        return queryset.none()
