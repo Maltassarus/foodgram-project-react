54f93b0ae7838a37c25e0dd8ba89e286563a09b9
Андрей Борзенко <borzenkoandreys@yandex.ru>
2023-06-18 11:07:37 +0300

    Исправил пагинацию

--- backend/api/paginator.py
+++ backend/api/paginator.py
@@ -3,13 +3,5 @@ from rest_framework.response import Response
 
 
 class PageNumberPaginator(PageNumberPagination):
-
-    def get_paginated_response(self, data):
-        return Response({
-            'links': {
-                'next': self.page.next_page_number,
-                'previous': self.page.previous_page_number,
-            },
-            'count': self.page.paginator.count,
-            'response': data,
-        })
+    page_size_query_param = "limit"
+    page_size = 6
